4e63d67112d533a2ab7dca0242145c8ff42dd521
Jugal Patel <jugalpatel@Jugals-MacBook-Pro.local>
2022-04-20 19:56:35 -0400

    Commit 3

--- src/org/jhotdraw/contrib/html/EllipseFigureGeometricAdapter.java
+++ src/org/jhotdraw/contrib/html/EllipseFigureGeometricAdapter.java
@@ -4,7 +4,7 @@
  * Project:		JHotdraw - a GUI framework for technical drawings
  *				http://www.jhotdraw.org
  *				http://jhotdraw.sourceforge.net
- * Copyright:	© by the original author(s) and all contributors
+ * Copyright:	ï¿½ by the original author(s) and all contributors
  * License:		Lesser GNU Public License (LGPL)
  *				http://www.opensource.org/licenses/lgpl-license.html
  */
@@ -49,6 +49,7 @@ public class EllipseFigureGeometricAdapter extends EllipseFigure
 	 *
 	 * @return   The shape value
 	 */
+	//TODO: Avoid using unnecessary local variables as this wastes memory
 	public Shape getShape() {
 		Rectangle rect = displayBox();
 		Ellipse2D.Float ellipse = new Ellipse2D.Float(rect.x, rect.y, rect.width, rect.height);
