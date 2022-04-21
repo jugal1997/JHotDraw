586a7e945ab04bfbb5359e7fc994927c1742ac51
Jugal Patel <jugalpatel@Jugals-MacBook-Pro.local>
2022-04-20 20:16:08 -0400

    Commit 9

--- src/org/jhotdraw/samples/javadraw/JavaDrawApplet.java
+++ src/org/jhotdraw/samples/javadraw/JavaDrawApplet.java
@@ -4,7 +4,7 @@
  * Project:		JHotdraw - a GUI framework for technical drawings
  *				http://www.jhotdraw.org
  *				http://jhotdraw.sourceforge.net
- * Copyright:	© by the original author(s) and all contributors
+ * Copyright:	ï¿½ by the original author(s) and all contributors
  * License:		Lesser GNU Public License (LGPL)
  *				http://www.opensource.org/licenses/lgpl-license.html
  */
@@ -121,7 +121,7 @@ public  class JavaDrawApplet extends DrawApplet {
 			fAnimationButton.setText("Start Animation");
 		}
 	}
-
+	//TODO: if-else statements must use curly braces as it will help in the future with maintainability
 	public void toggleAnimation() {
 		if (fAnimator != null)
 			endAnimation();
